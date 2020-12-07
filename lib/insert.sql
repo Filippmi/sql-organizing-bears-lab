1d126f197061ea3724caf319a9e6fd1cb4a07523
Filipp Minayev <Filippmi@gmail.com>
2020-12-07 16:01:35 -0700

    commit tree f732a6e4ddec3d7c8364465ed6f3d8a52c6fee7c

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr.Chocolate", 14, "M", "brown", "choleric", 1),
+(2, "Rowdy", 10, "M", "brown", "phlegmatic", 1),
+(3, "Tabitha", 4, "F", "black", "melancholic", 0),
+(4, "Sergeant Brown", 20, "M", "brown", "sanguine", 1),
+(5, "Melissa", 2, "F", "black", "choleric", 1),
+(6, "Grinch", 16, "M", "black", "sanguine", 0),
+(7, "Wendy", 12, "F", "brown", "phlegmatic", 1),
+(8, NULL, NULL, NULL, "brown", "choleric", 1);
